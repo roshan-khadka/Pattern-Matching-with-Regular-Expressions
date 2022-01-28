e863af40b4c93205020d1c6479fc3ee93c1c28c9
Roshan Khadka <65776857+roshan-khadka@users.noreply.github.com>
2022-01-28 10:59:04 +0200

    Create Phone and Email extractor.py

---
+++ Phone and Email extractor.py
@@ -0,0 +1,11 @@
+#: A simple program which finds phone numbers and email addresses
+
+# Create a regex for phone number.
+
+
+
+# Create a regex for email address.
+
+
+
+# Find all matched in the clipboard text.

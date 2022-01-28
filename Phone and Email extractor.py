5dcf1b5a9d6e9b578dc859fc88077c95fda6c52a
Roshan Khadka <65776857+roshan-khadka@users.noreply.github.com>
2022-01-28 12:14:16 +0200

    Update Phone and Email extractor.py

--- Phone and Email extractor.py
+++ Phone and Email extractor.py
@@ -1,8 +1,10 @@
 #: A simple program which finds phone numbers and email addresses
+import re
+import pyperclip # Pyperclip is a cross-platform Python module for copy and paste clipboard functions. It allows us to easily copy and paste from clipboard.
 
 # Create a regex for phone number.
-
-
+numregex = re.compile(r ''')
+#On progressssss!!!!!!!!!
 
 # Create a regex for email address.
 

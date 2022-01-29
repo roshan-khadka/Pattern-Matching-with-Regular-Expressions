c90d9c624df15ee798095a513a2c494aab6c705b
Roshan Khadka <65776857+roshan-khadka@users.noreply.github.com>
2022-01-29 10:30:48 +0200

    Update Phone and Email extractor.py

--- Phone and Email extractor.py
+++ Phone and Email extractor.py
@@ -3,11 +3,42 @@ import re
 import pyperclip # Pyperclip is a cross-platform Python module for copy and paste clipboard functions. It allows us to easily copy and paste from clipboard.
 
 # Create a regex for phone number.
-numregex = re.compile(r ''')
-#On progressssss!!!!!!!!!
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?                # area code
+    (\s|-|\.)?                        # separator
+    (\d{3})                           # first 3 digits
+    (\s|-|\.)                         # separator
+    (\d{4})                           # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?    # extension
+    )''', re.VERBOSE)
 
 # Create a regex for email address.
-
+emailRegex = re.compile(r'''(
+   [a-zA-Z0-9._%+-]+      # username
+   @                      # @ symbol
+   [a-zA-Z0-9.-]+         # domain name
+   (\.[a-zA-Z]{2,4})       # dot-something
+   )''', re.VERBOSE)
 
 
 # Find all matched in the clipboard text.
+
+text = str(pyperclip.paste())
+matches = []
+
+for groups in phoneRegex.findall(text):
+       phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+       if groups[8] != '':
+           phoneNum += ' x' + groups[8]
+       matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+       matches.append(groups[0])
+ 
+# Copy results to the clipboard.
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
